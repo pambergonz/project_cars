78ed326dff45b11b8bcc8ac2df4f55480b5eb9c7
pambergonz <pam.bergonz@gmail.com>
2019-10-14 19:23:47 -0300

    routes- brands and carModels

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/models','CarModelsController@index');
+Route::get('/model/create','CarModelsController@create');
+Route::get('/model/show/{id}','CarModelsController@show');
+Route::get('/model/edit/{id}','CarModelsController@edit');
+Route::post('/model/store','CarModelsController@store');
+Route::put('/model/update/{id}','CarModelsController@update');
+Route::delete('/model/destroy/{id}','CarModelsController@destroy');
+
+Route::get('/brands','BrandsController@index');
+Route::get('/brand/create','BrandsController@create');
+Route::get('/brand/show/{id}','BrandsController@show');
+Route::get('/brand/edit/{id}','BrandsController@edit');
+Route::post('/brand/store','BrandsController@store');
+Route::put('/brand/update/{id}','BrandsController@update');
+Route::delete('/brand/destroy/{id}','BrandsController@destroy');
