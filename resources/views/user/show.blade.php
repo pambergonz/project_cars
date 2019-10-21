21f3312213bf8b17312b89300f607b10c9e9c64c
pambergonz <pam.bergonz@gmail.com>
2019-10-21 15:48:03 -0300

    delete and update user buttons

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,6 +7,19 @@
   <div class="card m-5 t-5 text-center">
     <div class="card-header">
       <h4>Username:{{$user->username}}</h4>
+      <div class="btn-group">
+        <a href="/user/edit/{{$user->id}}"class="btn btn-primary">Update</a>
+
+      <form class="" action='/user/destroy/{{$user->id}}' method="post">
+      @csrf
+      {{method_field('DELETE')}}
+      <div class="row">
+      <div class="col-6">
+        <button type="delete" class="btn btn-danger">Delete</button>
+      </div>
+      </div>
+      </form>
+  </div>
     </div>
     <div class="card-body">
         <p>Name:{{$user->name}}</p>
@@ -14,9 +27,8 @@
     </div>
     <div class="card-footer">
       <h4>Privileges:{{$user->role}}</h4>
-      <div class="btn-group">
-        <a href="/user/edit/{{$user->id}}"class="btn btn-primary">Update</a>
-        <button type="button" class="btn btn-danger">Delete</button>
-      </div>
+
   </div>
+</div>
+
 @endsection
